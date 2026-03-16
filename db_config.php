c24ec0d0e034d17e6a9450d477392fbabecda550
Mohamed Farah <mohamedfarah@MacBook-Pro-2.local>
2026-03-15 23:51:15 -0700

    Test hardcoded db connection

--- db_config.php
+++ db_config.php
@@ -1,12 +1,15 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 $host = 'sql3.freesqldatabase.com';
 $user = 'sql3820170';
-$pass = '2heqKFvGs1';
+$pass = 'YOUR_ACTUAL_PASSWORD';
 $db   = 'sql3820170';
 
 $conn = new mysqli($host, $user, $pass, $db);
 
 if ($conn->connect_error) {
-    die('Connection failed: ' . $conn->connect_error);
+    die(json_encode(['error' => $conn->connect_error]));
 }
 ?>
