38a81eec3b72ad5ffa36aab948e038e90d689ef8
mffarah00 <141106451+mffarah00@users.noreply.github.com>
2026-03-15 23:51:43 -0700

    Update db_config.php

--- db_config.php
+++ db_config.php
@@ -10,6 +10,6 @@ $db   = 'sql3820170';
 $conn = new mysqli($host, $user, $pass, $db);
 
 if ($conn->connect_error) {
-    die(json_encode(['error' => $conn->connect_error]));
+  die(json_encode(['error' => $conn->connect_error]));
 }
 ?>
